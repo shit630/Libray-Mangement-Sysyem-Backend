5ea3810f2cfa4b613e4a8f3cfaf99584bf7f91b6
Soumen Shit <soumenshit907@gmail.com>
2025-10-01 14:05:51 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
