e1441b183c1d4009a708505701e87367b08a2107
Soumen Shit <soumenshit907@gmail.com>
2025-10-05 13:05:52 +0530

    email send fixed

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,7 +6,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
